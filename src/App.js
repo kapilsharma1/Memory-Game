ea11d6bc60b6dbb24bc52b19f94a2917f5f2adc4
Kapil <kapilsharma.8295@gmail.com>
2021-06-19 15:03:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+export default function App() {
+  const [openedCard, setOpenedCard] = useState([]);
+  const [matched, setMatched] = useState([]);
+  const [successTurns, setsuccessTurns] = useState(0);
+  const [turns, setTurns] = useState(0);
+  const shuffle = arr => arr.sort(() => 0.5 - Math.random());
+  const pokemons = [
+    { id: 1, name: "A" },
+    { id: 2, name: "B" },
+    { id: 3, name: "C" },
+    { id: 4, name: "D" },
+    { id: 5, name: "E" },
+    { id: 6, name: "F" },
+    { id: 7, name: "G" },
+    { id: 8, name: "H" }
+  ];
 
-function App() {
+
+  console.log(openedCard)
+  const [pairOfPokemons, setpairOfPokemons] = useState(shuffle([...pokemons, ...pokemons]));
+ 
+
+  function flipCard(index) {
+    if(openedCard[0])
+    {
+      if(openedCard[0]===index)
+      {
+        return;
+      }
+    }
+    setOpenedCard((opened) => [...opened, index]);
+  }
+  const alertUser = (e) => {
+    e.preventDefault();
+    e.returnValue = "";
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", alertUser);
+    // return () => {
+    //   window.removeEventListener("beforeunload", alertUser);
+    // };
+  }, []);
+
+
+
+  useEffect(() => {
+    //if (openedCard < 2) return;
+
+    const firstMatched = pairOfPokemons[openedCard[0]];
+    const secondMatched = pairOfPokemons[openedCard[1]];
+
+    if (secondMatched  &&firstMatched.id === secondMatched.id ) {
+      setMatched([...matched, firstMatched.id]);
+      setsuccessTurns(successTurns+1);
+    }
+    
+
+    if (openedCard.length === 2) 
+    {
+      setTimeout(() => setOpenedCard([]), 1000);
+      setTurns(turns+1);
+    }
+   
+  }, [openedCard]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between"}} >
+     <h1>Successful Matches: {successTurns}</h1>
+      <h1>Total Turns: {turns}</h1>
+     </div>
+
+      <div className="cards">
+      
+        {pairOfPokemons.map((pokemon, index) => {
+
+
+          let isFlipped = false;
+
+          if (openedCard.includes(index)) isFlipped = true;
+          if (matched.includes(pokemon.id)) isFlipped = true;
+          return (
+            <div
+              className={`pokemon-card ${isFlipped ? "flipped" : ""} `}
+              key={index}
+              onClick={() => flipCard(index)}
+            >
+              <div className="inner">
+                <div className="front">
+                  <img
+                    src={process.env.PUBLIC_URL + `/${pokemon.id}.png`}
+                    alt="pokemon-name"
+                    width="100"
+                  />
+                </div>
+                <div className="back"></div>
+              </div>
+              
+            </div>
+            
+          );
+        })}
+      </div>
+      <footer style={{textAlign:"center"}}>Made by Kapil Sharma</footer>
     </div>
   );
 }
-
-export default App;
